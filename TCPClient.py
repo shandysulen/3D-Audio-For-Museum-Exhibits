8a22086f230c78ad1c2912e72fe81611eaa7aa73
Paul <p.changfatt@gmail.com>
2018-04-16 22:07:20 -0400

    modify TCPclient to get hedge location data

--- TCPClient.py
+++ TCPClient.py
@@ -1,16 +1,16 @@
 import socket
-import pyaudio
-import wave
 import time
 
-HOST = '127.0.0.1'        # Symbolic name meaning all available interfaces
+HOST = '10.0.1.12'        # Symbolic name meaning all available interfaces
 PORT = 50007              # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 print('Successfully connected with ' + HOST + '...')
 
-s.sendall(b'1')
-data = s.recv(8192)
-print("Received " + str(len(data)) + " bytes...")
+while True:
+    s.sendall(b'1')
+    data = s.recv(8192)
+    print("Received " + str(len(data)) + " bytes...")
+    print(str(time.time()) + ": " + data.decode("utf-8"))
 
 s.close()
